4dc21d88d700f37fd288c72b5cc0ab01359d3591
ramorrison1 <64551570+ramorrison1@users.noreply.github.com>
2020-07-10 16:39:49 -0600

    Update ReferenceForm.js

--- src/components/Resume/ReferenceForm.js
+++ src/components/Resume/ReferenceForm.js
@@ -1,174 +1,155 @@
-
+// Updated 2020-07-08 14:00
 import React, { Component } from 'react';
 import  {Button, Form, Grid, Header, Segment} from 'semantic-ui-react';
-  
-  const styles = {
-    button:{
-        margin: 15
-    },
-    h1: {
-      marginTop: '3em',
-    },
-    h2: {
-      margin: '4em 0em 2em',
-    },
-    h3: {
-      marginTop: '2em',
-      padding: '2em 0em',
-    },
-    last: {
-      marginBottom: '300px',
-    },
-  }
+const uuid = require ('uuid');
 
 class ReferenceForm extends Component{
-  //Pagination
-    continue = e => {
-    e.preventDefault();
-    this.props.nextStep();
-    }
   
-    back = e => {
-      e.preventDefault();
-      this.props.prevStep();
-    }
 
   //Reference Form
     constructor(props) {
     super(props);
-    this.state = {contact_name:"", contact_lastname:"", contact_position:"", 
-    contact_email:"", contact_phone:"", 
-    company_name:"", contact_relationship:""};
+    this.state = {
+      contact_name:"",
+      contact_lastname:"",
+      contact_position:"", 
+      contact_email:"",
+      contact_phone:"", 
+      company_name:"",
+      contact_relationship:""};
     this.handleChange=this.handleChange.bind(this);
     this.handleSubmit=this.handleSubmit.bind(this);
   }
+
   handleChange(evt){
     this.setState({[evt.target.name]:evt.target.value});
   }
+
   handleSubmit(evt){
       evt.preventDefault();
+      this.props.newReference({...this.state, id: uuid.v4()});
+      this.setState({
+      contact_name:"",
+      contact_lastname:"",
+      contact_position:"", 
+      contact_email:"",
+      contact_phone:"", 
+      company_name:"",
+      contact_relationship:""
+      });
 
   }
+
   render(){
   return (
    
       <Form onSubmit={this.handleSubmit}>
-      <Header as= 'h2' content='stackable Grid' textAlign='center' style={styles.h3}> Do you have References?</Header>
-      <Grid columns={2} stackable>
-            
-            <Grid.Column>
-              <Segment>
-                <Form.Input fluid
-                    label="First Name"
-                    type='text'
-                    name='contact_name'
-                    placeholder='First Name'
-                    value={this.state.contact_name}
-                    onChange={this.handleChange}
-                />
-             </Segment>
-            </Grid.Column>
-
-            <Grid.Column>
-              <Segment>
-                <Form.Input fluid
-                    label="Last Name"
-                    type='text'
-                    name='contact_lastname'
-                    placeholder='Last Name'
-                    value={this.state.app_lastname} 
-                    onChange={this.handleChange}
+      <Header as= 'h2' content='stackable Grid' textAlign='center'> Do you have References?</Header>
+        <Grid columns={2} stackable>
+              
+              <Grid.Column>
+                <Segment>
+                  <Form.Input fluid
+                      label="First Name"
+                      type='text'
+                      name='contact_name'
+                      placeholder='First Name'
+                      value={this.state.contact_name}
+                      onChange={this.handleChange}
                   />
               </Segment>
-            </Grid.Column>        
+              </Grid.Column>
 
-          <Grid.Row> 
-            <Grid.Column>
-              <Segment>
-                <Form.Input fluid
-                    label='Email'
-                    type='email'
-                    name='contact_email'
-                    placeholder='Email'
-                    value={this.state.contact_email} 
-                    onChange={this.handleChange}
-                />
-              </Segment>
-            </Grid.Column>
+              <Grid.Column>
+                <Segment>
+                  <Form.Input fluid
+                      label="Last Name"
+                      type='text'
+                      name='contact_lastname'
+                      placeholder='Last Name'
+                      value={this.state.contact_lastname} 
+                      onChange={this.handleChange}
+                    />
+                </Segment>
+              </Grid.Column>        
 
-            <Grid.Column>
-              <Segment>
-                <Form.Input fluid
-                    label='Contact Phone Num.'
-                    type='tel'
-                    name='contact_phone'
-                    placeholder='Phone'
-                    value={this.state.contact_phone} 
-                    onChange={this.handleChange}
-                />
-              </Segment>
-            </Grid.Column>
-          </Grid.Row>
+            <Grid.Row> 
+              <Grid.Column>
+                <Segment>
+                  <Form.Input fluid
+                      label='Email'
+                      type='email'
+                      name='contact_email'
+                      placeholder='Email'
+                      value={this.state.contact_email} 
+                      onChange={this.handleChange}
+                  />
+                </Segment>
+              </Grid.Column>
 
-          <Grid.Row columns={1}>
-            <Grid.Column>
-              <Segment>
-                <Form.Input fluid
-                    label='Company Name'
-                    type='text'
-                    name='company_name'
-                    placeholder='Company/Organization'
-                    value={this.state.company_name} 
-                    onChange={this.handleChange}
-                />
-              </Segment>
-            </Grid.Column>
-          </Grid.Row>
+              <Grid.Column>
+                <Segment>
+                  <Form.Input fluid
+                      label='Contact Phone Num.'
+                      type='tel'
+                      name='contact_phone'
+                      placeholder='Phone'
+                      value={this.state.contact_phone} 
+                      onChange={this.handleChange}
+                  />
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
 
-          <Grid.Row columns={2} stackable> 
-            <Grid.Column>
-              <Segment>
-                <Form.Input fluid
-                    label='Relationship to Contact'
-                    type='text'
-                    name='contact_relationship'
-                    placeholder='e.g.,Former Manager'
-                    value={this.state.contact_relationship} 
-                    onChange={this.handleChange}
-                />
-                 </Segment>
-            </Grid.Column>
-  
-            <Grid.Column>
-              <Segment>
-                <Form.Input fluid
-                    label='Contact Position'
-                    type='text'
-                    name='contact_position'
-                    placeholder='e.g., Director of Marketing'
-                    value={this.state.contact_position}
-                    onChange={this.handleChange}
-                />
-              </Segment>
-            </Grid.Column>
-          </Grid.Row>
-      </Grid>
+            <Grid.Row columns={1}>
+              <Grid.Column>
+                <Segment>
+                  <Form.Input fluid
+                      label='Company Name'
+                      type='text'
+                      name='company_name'
+                      placeholder='Company/Organization'
+                      value={this.state.company_name} 
+                      onChange={this.handleChange}
+                  />
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
 
-        <Button
-          label="Back"
-          primary={false}
-          style={styles.button}
-          onClick={this.back}
-        />  
-        <Button
-          label="Next"
-          primary={true}
-          style={styles.button}
-          onClick={this.continue}
-        />
-   
-      </Form>
+            <Grid.Row columns={2} stackable> 
+              <Grid.Column>
+                <Segment>
+                  <Form.Input fluid
+                      label='Relationship to Contact'
+                      type='text'
+                      name='contact_relationship'
+                      placeholder='e.g.,Former Manager'
+                      value={this.state.contact_relationship} 
+                      onChange={this.handleChange}
+                  />
+                  </Segment>
+              </Grid.Column>
     
+              <Grid.Column>
+                <Segment>
+                  <Form.Input fluid
+                      label='Contact Position'
+                      type='text'
+                      name='contact_position'
+                      placeholder='e.g., Director of Marketing'
+                      value={this.state.contact_position}
+                      onChange={this.handleChange}
+                  />
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
+
+              <Grid.Column width={2}>
+              <Button color='blue'>Add </Button> 
+              </Grid.Column>
+
+        </Grid>
+      </Form> 
     );
   }
   
