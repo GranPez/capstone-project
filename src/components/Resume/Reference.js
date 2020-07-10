d9541ae9203286d59228632cc0e01f69bf947a17
ramorrison1 <64551570+ramorrison1@users.noreply.github.com>
2020-07-10 16:39:39 -0600

    Create Reference.js

---
+++ src/components/Resume/Reference.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import {Icon} from 'semantic-ui-react';
+
+class Reference extends Component {
+    constructor(props){
+        super(props);
+        this.handleRemoveReference = this.handleRemoveReference.bind(this);
+    }
+    handleRemoveReference() {
+        this.props.delReference(this.props.id);
+    }
+    render(){
+        return(
+            <div>
+                <button>Edit</button>
+                <button>Delete </button>         
+                <li>
+                  {this.props.contact_name}{``}
+                  {this.props.contact_lastname}{``}
+                  {this.props.contact_position}{``}
+                  {this.props.contact_email}{``}
+                  {this.props.contact_phone}{``}
+                  {this.props.company_name}{``}
+                  {this.props.contact_relationship}
+                </li>
+                <Icon link name='trash' color='red' onClick={this.handleRemoveReference} />
+            </div>
+        )
+    }
+}
+export default Reference;
