4e36121ea9649e8e466e5ae8994d617b5f671ae9
JFelipe-Moreno <j.moreno212@mybvc.ca>
2020-07-16 10:30:10 -0600

    Revert "render localstorage values (take 2)"
    This reverts commit 4a4bf715f0062a25b3809e24b365e5c203623686.

--- src/components/Resume/Template.js
+++ src/components/Resume/Template.js
@@ -1,46 +1,14 @@
-
 import React, { Component } from "react";
-// import  { Header} from 'semantic-ui-react';
-//import Box from './box'
+import Box from './box'
 
 class Template extends Component {
-
-    // componentDidMount() {
-    //     this.AboutYouInfo = JSON.parse(localStorage.getItem('contact'));
-     
-    // //     if (localStorage.getItem('aboutYou')) {
-    // //         this.setState({
-    // //             aboutYou: this.AboutYouInfo.aboutYou
-    // //     })
-    // // } else {
-    // //     this.setState({
-    // //       aboutYou:''
-    // //     })
-    // //   }
-    // }
-    
-
     render() {
-        //read localStorage and save key elements in variables
-        let qual = JSON.parse(localStorage.qualification);
-        let con = JSON.parse(localStorage.contact);
-        let edu = JSON.parse(localStorage.education);
-        // let qual = JSON.parse(localStorage.qualification);
-        // let qual = JSON.parse(localStorage.qualification);
-        // let qual = JSON.parse(localStorage.qualification);
-
-        
         return (
-
-            
             <div>
-                <hr></hr>
-                 <h1>{qual.qualification}</h1>
-                 <h1>{con.app_name}</h1>
-                 <h1>{con.app_lastname}</h1>
-                 <h1>{con.app_email}</h1>
-                 <h1>{edu.schoolName}</h1>
-
+                <Box height={74} width={236} color="grey" />
+                <Box height={74} width={500} color="grey" />
+                <Box height={74} width={236} color="grey" />
+                <Box height={74} width={236} color="grey" />
             </div>
         );
     }
