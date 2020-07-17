7448a6b25345dc84bf2aca0ad615728e54d2258f
jazmin199 <j.mendez199@mybvc.ca>
2020-07-17 09:47:07 -0600

    Updated (delete comments)

--- src/components/Resume/EducationForm.js
+++ src/components/Resume/EducationForm.js
@@ -5,9 +5,6 @@ import  {
 const uuid = require ('uuid');
 //import {Form} from 'semantic-ui-react-form-validator';
 
-//import PropTypes from 'prop-types';
-//import { connect } from 'react-redux';
-//import { render } from '@testing-library/react';
 
 const degrees = [
     {key:'BA', text: 'Bachelor of Arts', value:'Bachelor of Arts'},
@@ -201,39 +198,3 @@ export default EducationForm;
 
 
 
-/*const Education = ({ education, font }) => (
-  <section data-testid="Education" className="resume-education">
-    <h2 style={{ fontFamily: font }}>Education</h2>
-    <hr />
-    <ul>
-      {education.map(
-        ed => ed.isVisible !== false && (
-        <li key={uuid()}>
-          <h3 style={{ fontFamily: font }}>{ed.site}</h3>
-          {ed.dateFrom &&
-            <h3 style={{ fontFamily: font }}>
-              {`${ed.dateFrom}${ed.dateTo ? ` - ${ed.dateTo}` : ''}`}
-            </h3>}
-          <em>{ed.studyDegree}</em>
-        </li>
-        ),
-      )}
-    </ul>
-  </section>
-);
-
-Education.defaultProps = {
-  education: [],
-};
-
-Education.propTypes = {
-  education: PropTypes.arrayOf(PropTypes.shape({})),
-  font: PropTypes.string.isRequired,
-};
-
-const mapStateToProps = state => ({
-  education: state.resume.education,
-  font: state.tools.font,
-});
-
-export default connect(mapStateToProps)(Education);*/
